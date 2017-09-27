42e481a9b37d602f77dc030c09558f88f8201b66
Alan Grover <awgrover@gmail.com>
2017-09-27 12:54:17 -0400

    change output node name

--- lib/awg/logging/text/any.cpp
+++ lib/awg/logging/text/any.cpp
@@ -11,23 +11,17 @@ void evaluate(Context ctx) {
     if (isInputDirty<input_text>(ctx)) {
       newvalue = getValue<input_text>(ctx);
       // emit always || (input_text changed)
-      should_emit = always || (newvalue != getValue<output_text>(ctx));
+      should_emit = always || (newvalue != getValue<output_copy>(ctx));
       }
     else {
-      newvalue = getValue<output_text>(ctx);
+      newvalue = getValue<output_copy>(ctx);
       // emit based on always (text didn't change, but always might have)
       should_emit = always;
       }
 
     if (should_emit) {
-      if (newvalue) {
-          for (auto it = newvalue->iterate(); it; ++it)
-              Serial.write((char)*it);
-          Serial.write('\r');
-          Serial.write('\n');
-          Serial.flush();
-      }
-
+      emitValue<output_copy>(ctx, newvalue);
+      emitValue<output_trig>(ctx, 1);
       }
 
 }
